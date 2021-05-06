4f485283baac079835b4a27fac21d5732da52a9a
Ludwig <hilzingercristian@hotmail.com>
2021-05-06 14:56:59 -0300

    hello world en c++

--- Mira_mami.c
+++ Mira_mami.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Mira Mami... SIN MANOS!!!");
+   printf("Mira mami... SIN MANOS!!!");
    return 0;
 }
---
+++ Mira_mami.cpp
@@ -0,0 +1,8 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+    cout<<"Mira mami... sin manos";
+    return 0;
+}
